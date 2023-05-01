afa0fc867760b65a5c8460a95d78c517739e20c0
Variable2831 <44816513+Variable2831@users.noreply.github.com>
2023-05-01 15:51:29 +0100

    Create array-utils.js

---
+++ src/array-utils.js
@@ -0,0 +1,5 @@
+import { shuffleArray } from './src/array-utils.js';
+
+const myArray = [1, 2, 3, 4, 5];
+const shuffledArray = shuffleArray(myArray);
+console.log(shuffledArray); // Output: [5, 2, 1, 4, 3]
