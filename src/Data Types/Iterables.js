e0d3c0ae99967193a979e04c2e5162a273980e71
Variable2831 <44816513+Variable2831@users.noreply.github.com>
2023-05-01 17:29:24 +0100

    Updated

---
+++ src/Data Types/Iterables.js
@@ -0,0 +1,25 @@
+//1. An array of colors:
+let colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"];
+
+//2. An array of numbers:
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//3. An array of objects:
+let people = [
+  { name: "John", age: 20 },
+  { name: "Jane", age: 30 },
+  { name: "Bob", age: 40 },
+  { name: "Mary", age: 50 }
+];
+
+//4. A string of characters:
+let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+//5. An array of words:
+let words = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+
+console.log(colors);
+console.log(numbers);
+console.log(people);
+console.log(characters);
+console.log(words);
