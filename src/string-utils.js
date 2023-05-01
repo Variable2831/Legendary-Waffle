ed71bcc97f5ea1058f4cf9fd3f439ce99efb9e11
Variable2831 <44816513+Variable2831@users.noreply.github.com>
2023-05-01 15:52:08 +0100

    Create string-utils.js

---
+++ src/string-utils.js
@@ -0,0 +1,5 @@
+import { capitalizeString } from './src/string-utils.js';
+
+const myString = 'hello world';
+const capitalizedString = capitalizeString(myString);
+console.log(capitalizedString); // Output: 'Hello world'
